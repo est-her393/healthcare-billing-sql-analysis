33f897c06543711d24a293f2ed0f3a09cd7a992f
est-her393 <estherd393@gmail.com>
2026-06-15 16:13:21 -0400

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,2 +1,31 @@
+#total revenue 
 SELECT SUM(charge_amount) AS total_revenue
 FROM billing_data;
+
+#Claims Status
+SELECT claim_status, COUNT(*) AS total_claims
+FROM billing_data
+GROUP BY claim_status;
+
+#Denied Claims
+SELECT *
+FROM billing_data
+WHERE claim_status = 'Denied';
+
+#Error Rate Analysis
+SELECT 
+    error_flag,
+    COUNT(*) AS count
+FROM billing_data
+GROUP BY error_flag;
+
+#Most Expensive Procedures
+SELECT procedure, AVG(charge_amount) AS avg_cost
+FROM billing_data
+GROUP BY procedure
+ORDER BY avg_cost DESC;
+
+#Insurance vs Uninsured Revenue
+SELECT insurance_status, SUM(charge_amount) AS total_revenue
+FROM billing_data
+GROUP BY insurance_status;
